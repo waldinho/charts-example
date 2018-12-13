28a648e776759600f6402cf42d212435a982c65b
waldinho <james.waller1984@gmail.com>
2018-12-13 12:01:26 +1100

    radar added.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Line} from 'react-chartjs-2';
+import {Line, Radar} from 'react-chartjs-2';
 
 class Chart extends Component {
 
@@ -34,16 +34,39 @@ class Chart extends Component {
         }
       )
   }
-
   
   render() {
     
     const { labels, price } = this.state;
     const data = {
-      labels: labels,
+      // labels: labels,
+      // datasets: [
+      //   {
+      //     // label: 'Yearly Price in $AUD',
+      //     // fill: false,
+      //     lineTension: 0.1,
+      //     backgroundColor: 'rgba(75,192,192,0.4)',
+      //     borderColor: 'rgba(75,192,192,1)',
+      //     // borderCapStyle: 'butt',
+      //     // borderDash: [],
+      //     // borderDashOffset: 0.0,
+      //     // borderJoinStyle: 'miter',
+      //     // pointBorderColor: 'rgba(75,192,192,1)',
+      //     // pointBackgroundColor: '#fff',
+      //     // pointBorderWidth: 1,
+      //     // pointHoverRadius: 5,
+      //     // pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      //     // pointHoverBorderColor: 'rgba(220,220,220,1)',
+      //     // pointHoverBorderWidth: 2,
+      //     // pointRadius: 1,
+      //     // pointHitRadius: 10,
+      //     data: price,
+      //   }
+      // ]
+      labels: [['React', ' ',], ['            Node'], [' ', ' ', 'HTML'], [' ', ' ', 'CSS'], ['Javascript            ']],
       datasets: [
         {
-          label: 'Yearly Price in $AUD',
+          // label: 'Yearly Price in $AUD',
           // fill: false,
           lineTension: 0.1,
           backgroundColor: 'rgba(75,192,192,0.4)',
@@ -61,14 +84,57 @@ class Chart extends Component {
           // pointHoverBorderWidth: 2,
           // pointRadius: 1,
           // pointHitRadius: 10,
-          data: price,
+          data: [80, 70, 95, 95, 75],
+        }, {
+          label: 'Hidden dataset',
+          hidden: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(0,0,0,0)',
+          borderColor: 'rgba(0,0,0,0)',
+          // borderCapStyle: 'butt',
+          // borderDash: [],
+          // borderDashOffset: 0.0,
+          // borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(0,0,0,0)',
+          // pointBackgroundColor: '#fff',
+          pointBorderWidth: 0,
+          pointHoverRadius: 0,
+          pointHoverBackgroundColor: 'rgba(0,0,0,0)',
+          pointHoverBorderColor: 'rgba(0,0,0,0)',
+          pointHoverBorderWidth: 0,
+          pointRadius: 0,
+          pointHitRadius: 0,
+          data: [50, 100],
         }
       ]
-    };
+    }
     return (
       <div>
         <h2>Line Example</h2>
-        <Line data={data} />
+        {/* <Line 
+          id='chart'
+          data={data} 
+          legend={{display: false}}
+          width={100}
+          height={45}
+          redraw={true}
+        /> */}
+        <Radar 
+          id='chart'
+          data={data} 
+          legend={{display: false}}
+          width={100}
+          height={40}
+          redraw={true}
+          options={{
+            scale: {
+              pointLabels : { 
+                 fontSize: 14,
+                 fontColor: 'red',
+              },
+            },
+          }}
+        />
       </div>
     );
   }
